eb7574ec7c08f95713037285a20ec9cc12b5dc61
shivam kumar mishra <shivammishra692002@gmail.com>
2023-07-18 12:21:30 +0530

    Added make_response()

--- Project/model/userModel.py
+++ Project/model/userModel.py
@@ -11,7 +11,6 @@ class userModel():
             self.con=mysql.connector.connect(host="localhost",username="root",password="P@ssword191394",database="flask_project")
             self.con.autocommit=True
             self.cur=self.con.cursor(dictionary=True)
-            print("successful")
 
         except:
             print("some error")
@@ -25,32 +24,32 @@ class userModel():
         self.cur.execute("SELECT * FROM USERS")
         result = self.cur.fetchall()
         if(len(result)>0):
-            return {"payload":result}
+            return make_response({"payload":result},200)
 
             ''' 
             json.dumps(result): this return the result in list formate
             '''
         else:
-            return { "Message":"No data"}
+            return make_response({ "Message":"No data"},204)
 
     def add_user(self,data):
         self.cur.execute(f"INSERT INTO users(name,email,phone,role,password) VALUES('{data['name']}','{data['email']}','{data['phone']}','{data['role']}','{data['password']}'   )")
-        return { "Message":"User Added"}
+        return make_response({ "Message":"User Added"},201)
         
         
     def update_user(self,data):
         # print(data)
         self.cur.execute(f"UPDATE users SET name='{data['name']}' ,email='{data['email']}' ,phone='{data['phone']}' ,role ='{data['role']}' ,password='{data['password']}' WHERE id={data['id']}")
         if self.cur.rowcount>0:
-            return { "Message":"User Updated"}
+            return make_response({ "Message":"User Updated"},201)
         else:
-            return { "Message":"nothing to update"}
+            return make_response({ "Message":"nothing to update"},202)
     
     def delete_user(self,id):
         # print(data)
         self.cur.execute(f"DELETE FROM users WHERE id={id}")
         if self.cur.rowcount>0:
-            return { "Message":"User Deleted"}
+            return make_response({ "Message":"User Deleted"},200)
         else:
-            return { "Message":"nothing to nothingf to delete"}
+            return make_response({ "Message":"nothing to nothingf to delete"},202)
     
